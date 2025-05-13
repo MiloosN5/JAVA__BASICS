deb0cc3a45d27c46adc6cbda7c829355f838781e
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2025-05-13 11:18:23 +0200

    Interface example

---
+++ src/references/Vehicle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package references;
+
+/**
+ *
+ * @author PC
+ */
+public interface Vehicle {
+
+    void accelerate(int value);
+
+    void brake(int value);
+
+    void changeGear(int gear);
+}
