94398c300f006e73e457ebcb02a6d5678891e54a
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2025-11-09 12:44:15 +0100

    updating 'inhteritance' example
    adding example for using parent class reference for object made of child class

--- src/main/java/lessons/InheritanceExample.java
+++ src/main/java/lessons/InheritanceExample.java
@@ -35,15 +35,36 @@ public class InheritanceExample {
     }
 
     public static void run() {
-        MountainBike myBike = new MountainBike(10, 0, 1, 5);
-        myBike.changeGear(2);
-        myBike.speedUp(20);
-        myBike.changeCadence(90);
-        myBike.setSuspension(7);
-        myBike.printFullState();
+        // [1] Using child class reference: all MountainBike methods are accessible
+        MountainBike myBike1 = new MountainBike(10, 0, 1, 5);
+        myBike1.changeGear(2);
+        myBike1.speedUp(20);
+        myBike1.changeCadence(90);
+        myBike1.setSuspension(7);
+        myBike1.printFullState();
         /*
             cadence:90 speed:20 gear:2
             suspension level: 7
-         */
+        */
+        
+        // [2] Using parent class reference: only Bicycle methods are accessible directly
+        Bicycle myBike2 = new MountainBike(10, 0, 1, 5);
+        myBike2.changeGear(2);
+        myBike2.speedUp(20);
+        myBike2.changeCadence(90);
+        // myBike2.setSuspension(7); // variable myBike2 of type references.classes.Bicycle
+        // myBike2.printFullState(); // variable myBike2 of type references.classes.Bicycle
+    
+        // to use methods specific to "MountainBike", we need to cast the reference
+        if (myBike2 instanceof MountainBike) {
+            MountainBike mtb = (MountainBike) myBike2;
+            mtb.setSuspension(7);
+            mtb.printFullState(); 
+        }
+        /*
+            cadence:90 speed:20 gear:2
+            suspension level: 7
+        */       
+    
     }
 }
